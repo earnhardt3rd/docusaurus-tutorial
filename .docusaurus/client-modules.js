3b37c5a69609afa4d97c831b9167391fa6908c16
frank.earnhardt <frank.earnhardt@outlook.com>
2023-04-17 00:20:40 -0400

    Deploy website - based on 3731e2130d0db20ad7b82461c3a7d53c74a2f104

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('E:\\work\\myDocusaurus\\docusaurus-tutorial\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('E:\\work\\myDocusaurus\\docusaurus-tutorial\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('E:\\work\\myDocusaurus\\docusaurus-tutorial\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('E:\\work\\myDocusaurus\\docusaurus-tutorial\\src\\css\\custom.css'),
-];
